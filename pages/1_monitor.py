ad9d375298da187f9b5e286b9a250680733d7433
vibesapuka-art <vibes.apuka@gmail.com>
2025-11-23 09:19:31 -0300

    Create 1_monitor.py

---
+++ pages/1_monitor.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Monitor de Teste")
+st.title("Monitor - Teste OK")
+st.subheader("Se você está vendo isso, o Monitor está funcionando!")
