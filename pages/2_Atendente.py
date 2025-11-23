8545036e1d46eb3d5748f09ae3da38dcb4e42812
vibesapuka-art <vibes.apuka@gmail.com>
2025-11-23 09:51:01 -0300

    Delete pages directory

--- pages/2_Atendente.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide", page_title="Atendente de Teste")
-st.title("Atendente - Teste OK")
-st.subheader("Se você está vendo isso, o Atendente está funcionando!")
